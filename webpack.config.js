8b08bfbc1ff0038c0792c1edec4f82fccea9e6ce
chenxiaofen <852076508@qq.com>
2019-12-09 18:25:28 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
-var path = require('path')
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'App',
+      template: 'src/assets/template.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
