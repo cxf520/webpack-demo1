81c15f2cc6c0554fa4c23909db92242e474c099b
chenxiaofen <852076508@qq.com>
2019-12-13 15:09:12 +0800

    update code

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 yarn build &&
 git checkout gh-pages &&
-rm -rf *.html *.js *.css *.png yarn.lock *.json *.sh &&
+rm -r *.html *.js &&
 mv dist/* ./ &&
-rm -r dist;
+rm -rf dist;
 git add . &&
 git commit -m 'update1' &&
 git push
